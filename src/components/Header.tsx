f12c0deb57daa5ee59f00cd59dca869dffa60f68
khafidhkayyis <khafidhkayyis@gmail.com>
2024-05-31 22:46:01 +0700

    nextJs-install

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header>
+            <Link
+                className="text-blue-600"
+                href="">
+                marketplace
+            </Link>
+        </header>
+    )
+}

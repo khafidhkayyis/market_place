1be58b92de628277801b6c65aa4227fca7aa390f
khafidhkayyis <khafidhkayyis@gmail.com>
2024-06-20 22:23:36 +0700

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function Header() {
@@ -8,12 +10,14 @@ export default function Header() {
                 href="/">
                 Marketplace
             </Link>
-            <nav className="flex gap-4 *:border *:rounded *:px-2 *:py-1">
-                <button className="border-blue-600 text-blue-600">
-                    Post a add
+            <nav className="flex gap-4 *:rounded *:py-1">
+                <button className=" border border-blue-600 text-blue-600 inline-flex gap-1 items-center px-2 mr-4">
+                    <FontAwesomeIcon icon={faPlus} className="h-4" />
+                    <span>Post a add</span>
                 </button>
-                <button className="">Login</button>
-                <button className="">Register</button>
+                <span className="border-r"></span>
+                <button className="border-0 text-gray-600">Sign up</button>
+                <button className="bg-blue-600 text-white border-0 px-6">Login</button>
             </nav>
         </header>
     )
